38d8ea3fa93fcaca06cbd4c421f9e57cf654836e
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2021-12-13 09:27:51 +0100

    Add files via upload

---
+++ ZadatakZaOcjenuXMLERDGabrielMatosevic20211213/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ZadatakZaOcjenuXMLERDGabrielMatosevic20211213
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UnosKorisnikaButun = new System.Windows.Forms.Button();
+            this.UnosKnjigeButun = new System.Windows.Forms.Button();
+            this.UnosPosudivanjaButun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UnosKorisnikaButun
+            // 
+            this.UnosKorisnikaButun.Location = new System.Drawing.Point(249, 96);
+            this.UnosKorisnikaButun.Name = "UnosKorisnikaButun";
+            this.UnosKorisnikaButun.Size = new System.Drawing.Size(206, 23);
+            this.UnosKorisnikaButun.TabIndex = 0;
+            this.UnosKorisnikaButun.Text = "Unos Korisnika";
+            this.UnosKorisnikaButun.UseVisualStyleBackColor = true;
+            this.UnosKorisnikaButun.Click += new System.EventHandler(this.UnosKorisnikaButun_Click);
+            // 
+            // UnosKnjigeButun
+            // 
+            this.UnosKnjigeButun.Location = new System.Drawing.Point(249, 125);
+            this.UnosKnjigeButun.Name = "UnosKnjigeButun";
+            this.UnosKnjigeButun.Size = new System.Drawing.Size(206, 23);
+            this.UnosKnjigeButun.TabIndex = 1;
+            this.UnosKnjigeButun.Text = "Unos Knjige";
+            this.UnosKnjigeButun.UseVisualStyleBackColor = true;
+            this.UnosKnjigeButun.Click += new System.EventHandler(this.UnosKnjigeButun_Click);
+            // 
+            // UnosPosudivanjaButun
+            // 
+            this.UnosPosudivanjaButun.Location = new System.Drawing.Point(249, 154);
+            this.UnosPosudivanjaButun.Name = "UnosPosudivanjaButun";
+            this.UnosPosudivanjaButun.Size = new System.Drawing.Size(206, 23);
+            this.UnosPosudivanjaButun.TabIndex = 2;
+            this.UnosPosudivanjaButun.Text = "Unos Posudivanja/Vracanja";
+            this.UnosPosudivanjaButun.UseVisualStyleBackColor = true;
+            this.UnosPosudivanjaButun.Click += new System.EventHandler(this.UnosPosudivanjaButun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UnosPosudivanjaButun);
+            this.Controls.Add(this.UnosKnjigeButun);
+            this.Controls.Add(this.UnosKorisnikaButun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UnosKorisnikaButun;
+        private System.Windows.Forms.Button UnosKnjigeButun;
+        private System.Windows.Forms.Button UnosPosudivanjaButun;
+    }
+}
+
---
+++ ZadatakZaOcjenuXMLERDGabrielMatosevic20211213/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZadatakZaOcjenuXMLERDGabrielMatosevic20211213
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UnosKorisnikaButun_Click(object sender, EventArgs e)
+        {
+            UnosKorisnika NoviKorisnik = new UnosKorisnika();
+            NoviKorisnik.ShowDialog();
+        }
+
+        private void UnosKnjigeButun_Click(object sender, EventArgs e)
+        {
+            UnosKnjige NovaKnjiga = new UnosKnjige();
+            NovaKnjiga.ShowDialog();
+        }
+
+        private void UnosPosudivanjaButun_Click(object sender, EventArgs e)
+        {
+            UnosPosudivanja NovoPosudivanje = new UnosPosudivanja();
+            NovoPosudivanje.ShowDialog();
+        }
+    }
+}
